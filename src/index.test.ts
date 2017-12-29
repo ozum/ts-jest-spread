74ed464a44ad684418effb00bb0e7e58b4d338fb
Ozum <ozum@ozum.net>
2017-12-29 16:43:45 +0300

    Initial

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import { A } from "./index";
+
+describe("json-query-parser", () => {
+  it("should parse key & value", done => {
+    const a = new A();
+    expect(1).toEqual(1);
+    done();
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+class A {
+  constructor() {
+    const a = { a: 1 };
+    const b = { b: 1, ...a };
+  }
+}
+
+export { A };
